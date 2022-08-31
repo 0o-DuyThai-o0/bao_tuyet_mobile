fe4ee324a41a0d77d32a56b47a39485548e9af51
0o-DuyThai-o0 <95570970+0o-DuyThai-o0@users.noreply.github.com>
2022-09-01 01:27:32 +0700

    create bao tuyet mobile

---
+++ js/index2.js
@@ -0,0 +1,16 @@
+var swiper2 = new Swiper(".mySwiper2", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    freeMode: true,
+    pagination: {
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
